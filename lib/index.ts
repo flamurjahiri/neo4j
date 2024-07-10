7b87ab136d99f05d0f9feb5964f428f94bdf8260
flamurjahiri <flamurjahiri@kodelabs.com>
2024-07-10 21:14:48 +0200

    exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 export * from './neo4j.module';
 export * from './decorators/neo4j.database.decorator'
+export * from './health/health.service'
+export * from './services/neo4j.database.service'
+export * from './processor/neo4j.operation.type'
 
